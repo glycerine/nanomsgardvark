ffbaa3e87398f98bfa7f18f320aca4eabb1320b7
Jason E. Aten <j.e.aten@gmail.com>
2014-12-21 18:10:50 +0000

    atg. update to remote/tcp/grep test

--- tests/listenserve.R
+++ tests/listenserve.R
@@ -7,7 +7,7 @@ library(nanomsgardvark)
 # nanomsg endpoint to use in tests cases.
 #test.ENDPOINT <- "inproc://poll"
 #test.ENDPOINT <- "ipc:///tmp/test.ipc"
-test.ENDPOINT <- "tcp://127.0.0.1:5551"
+test.ENDPOINT <- "tcp://0.0.0.0:5551"
 
 # Testing helpers.
 assert <- function(condition, message="Assertion Failed") if(!condition) stop(message)
@@ -24,10 +24,19 @@ assert.fails <- function(expr, message="Assertion Failed") {
     assert(rc > 0, "nn.bind() should return positive endpoint id")
 
     grover=0
+    found=0
     handler = function(msg) {
        #cat(file=stderr(),"hanlder() called, on err\n")
        #print(paste(sep="","handler() sees msg = '", unserialize(msg),"'"))
        grover<<-grover+1
+       umsg = unserialize(msg)
+       g = grep("Hello.*World",umsg)
+       if (length(g) > 0) {
+          found <<- found+1
+          if (found %% 1000 == 0) {
+             #print(paste(sep="", "found is at: ",found))
+          }
+       }
     }
 
     print(paste("listening on ", test.ENDPOINT,"and calling back to handler"))
